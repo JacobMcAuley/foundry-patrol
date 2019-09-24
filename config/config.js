7e95cddd2df7fe0b8687641e1e062816c20e2c19
JacobMcAuley <mcauleyj@uci.edu>
2019-09-23 23:35:51 -0700

    Added configuration options for non-GM owned tokens

---
+++ config/config.js
@@ -0,0 +1,13 @@
+PATROLCONFIG = {
+    module : "foundry-patrol",
+    key : "enablePlayerPatrol",
+    settings : {
+        name : "Player Access",
+        hint: "Enables patrol access for player owned tokens.",
+        scope: "world",
+        config: true,
+        default: true,
+        type: Boolean,
+        choices: undefined
+    }
+}
