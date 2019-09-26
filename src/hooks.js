191492ba26bc6fa7014029daead5b00a813630ee
JacobMcAuley <mcauleyj@uci.edu>
2019-09-25 18:48:03 -0700

    Tokens before the mod was enabled should now be properly added

--- src/hooks.js
+++ src/hooks.js
@@ -5,7 +5,11 @@ Hooks.on('renderTokenHUD', (app, html, data) => tokenHUDPatrol(app,html,data));
 Hooks.on('canvasInit', () => {
     Token.prototype.routes = null;
     let tokens = canvas.tokens.ownedTokens;
-    for(let i = 0; i < tokens.length; i++){
+    for(let i = 0; i < tokens.length; i++){ 
+        if(tokens[i].data.flags["foundry-patrol"] == null){
+            setProperty(tokens[i].data, "flags.foundry-patrol.routes", {});
+            tokens[i].update(canvas.scene.id, JSON.parse(JSON.stringify(tokens[i].data))) // I couldn't think of how to do this and then I saw Felix' solution. Thanks!   
+        }
         if(tokens[i].routes == null)
         {
             tokens[i].routes = new Patrols(tokens[i]);
@@ -20,3 +24,7 @@ Hooks.on('createToken', function(token, sceneId, data){
 Hooks.on("preCreateToken", (actorId, createData, options) => {
     setProperty(options, "flags.foundry-patrol.routes", {});
 });
+
+Hooks.on("deleteToken",(token, sceneId, options) =>{
+    token.routes.isDeleted = true;
+})
